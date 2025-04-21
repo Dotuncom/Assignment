bf4df1d226ebf4592cf6989c9beb28deb6dfaee6
Dotun Balogun <balogunlightonola@gmail.com>
2025-04-21 11:25:32 +0100

    footer

--- My9jaMeals/src/Homepage/Home.jsx
+++ My9jaMeals/src/Homepage/Home.jsx
@@ -5,12 +5,16 @@ import Footer from "../UlitiliComponents/Footer";
 const Home = () => {
   return (
     <div>
-      <div className="bg-gray-50 max-w-full max-h-full overflow-x-hidden">
+      <div className="bg-gray-50 max-w-full max-h-full ">
+        
         <Navbar />
         <main className="pt-16">
-        <FoodPreview />
-        <Footer/>
-
+        <p className="font-medium text-base md:font-bold md:text-2xl md:text-center">
+          Welcome to 9ja Meals where you can <br>
+          </br>get your meals at convenient prices
+        </p>
+          <FoodPreview />
+          <Footer />
         </main>
       </div>
     </div>
--- My9jaMeals/src/UlitiliComponents/Footer.jsx
+++ My9jaMeals/src/UlitiliComponents/Footer.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import nhub from '../assets/nhub.png'
 
 const Footer = () => {
   return (
-    <div>
-        <div className= 'bg-green-300 text-center text-white '>
-            @copyright dotun
-        </div>
-    </div>
+    <div className='flex flex-col justify-between md:flex-row md:p-10 items-center bg-green-500  sm:h-[14rem] p-4'>
+      <div className='flex flex-col sm:flex-col gap-4 items-center' >
+        <p className='text-white'>quick link</p>
+        <a href="#" className="hover:text-yellow-300 transition-colors">Home</a>
+          <a href="#" className="hover:text-yellow-300 transition-colors">About</a>
+          <a href="#" className="hover:text-yellow-300 transition-colors">Meals</a>
+          <a href="#" className="hover:text-yellow-300 transition-colors">Contact</a>
+      </div>
+      <div className='flex flex-row justify-center items-center md:flex-col md:text-2xl'>
+        <img src={nhub} width={60} className='rounded-full'/>
+        <p className='text-center text-[12px]'>all right reserved copyright@nhub dotun</p>
+      </div>
+  </div>
   )
 }
 
